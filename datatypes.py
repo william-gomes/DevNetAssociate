9ef36019b49bb0cc8968dc9120c9369e00b8b796
William Gomes <william.rko@gmail.com>
2025-05-26 22:33:40 -0300

    Trabalhando com sequencias

--- datatypes.py
+++ datatypes.py
@@ -1,8 +1,32 @@
-my_first_var = 42
-my_second_var = None
-my_third_var = 24
+my_list = [
+    1, 
+    2, 
+    223, 
+    45, 
+    866,
+    True,
+    'Ola Mundo',
+    None
+]
 
-print (my_first_var+my_second_var+my_third_var)
-# This code will raise an error because None cannot be added to an integer.
+#print(my_list[7])
+# my_list[-1] = 'Banana'
+# print(my_list)
 
+my_taple = (
+    1, 
+    2, 
+    223, 
+    45, 
+    866,
+    True,
+    'Ola Mundo',
+    None
+)
 
+# print(my_taple[7])
+# my_taple[3] = 'Banana'  # This will raise an error because tuples are immutable
+
+my_first_string = 'Ola Mundo'
+print (my_first_string[2])  # Accessing the four character
+print (my_first_string[2:7])  # Slicing the string from index 2 to 7
