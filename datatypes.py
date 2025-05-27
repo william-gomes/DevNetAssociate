ebd4ba6ee7b00e5f5b7483c303dbf574d6fdd0fc
William Gomes <william.rko@gmail.com>
2025-05-26 23:13:56 -0300

    Challenge, trabalhando com lista e dicts

--- datatypes.py
+++ datatypes.py
@@ -1,23 +1,42 @@
-my_first_person = {
-    "name": "William",
-    "surname": "Gomes",
-    "age": 31,
-    "is_happy": True,
-    "enemies": [None]
-}
+meus_carros = [
+    {
+        "marca": "Jeep",
+        "modelo": "Compass",
+        "ano": 2022,
+        "preco_pago": 140000.00,
+        "vendido": False,
+        "caracteristicas": {
+            "cor": "Prata",
+            "transmissao": "Automática",
+            "combustivel": "Flex"
+        }
+    },
+    {
+        "marca": "Honda",
+        "modelo": "City",
+        "ano": 2013,
+        "preco_pago": 51000.00,
+        "vendido": True,
+        "caracteristicas": {
+            "cor": "Prata",
+            "transmissao": "Automática",
+            "combustivel": "Flex"
+        }
+    },
+    {
+        "marca": "Fiat",
+        "modelo": "UNO",
+        "ano": 2000,
+        "preco_pago": 10000.00,
+        "vendido": True,
+        "caracteristicas": {
+            "cor": "Vinho",
+            "transmissao": "Manual",
+            "combustivel": "Gasolina"
+        }
+    }
+]
 
-# print(my_first_person["surname"])  # This will raise a KeyError because 'surname' is not defined as a variable
-#print(my_first_person["surname"],my_first_person["age"])  # This will print the surname and age of the person
-
-my_second_person = {
-    "name": "John",
-    "surname": "Doe",
-    "age": 25,
-    "is_happy": False,
-    "enemies": [my_first_person]
-}
-my_first_list = [my_first_person, my_second_person]
-
-# print(my_first_list[0]["name"])  # This will print the name of the first person in the list
-# print(my_first_list[1]["enemies"][0]["name"])  # This will print the name of the first person's enemy
-print (my_first_list)
+print(meus_carros[0]["marca"])  # Acessando a marca do primeiro carro
+print(meus_carros[1]["caracteristicas"]["cor"])  # Acessando a cor do segundo carro
+print(meus_carros[2]["vendido"])  # Verificando se o terceiro carro foi vendido
