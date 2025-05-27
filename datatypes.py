248d67974d882ecf1a68c0e88945b1b090621fe6
William Gomes <william.rko@gmail.com>
2025-05-26 21:45:30 -0300

    Variaveis numbers

--- datatypes.py
+++ datatypes.py
@@ -1,24 +1,8 @@
-my_variable = 42
-print(my_variable)
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (" ")
-print (my_variable)
+my_first_int = 42
+my_second_int = 100
+my_first_float = 3.14
+my_second_float = 2.718
+
+print (my_first_float+my_second_float)
+print (my_first_int+my_second_float)
+print (type(my_first_float+my_first_int))
