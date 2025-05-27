83bff7311304af87e08218adbd303a37e84d5779
William Gomes <william.rko@gmail.com>
2025-05-26 22:10:29 -0300

    Testando booelans

--- datatypes.py
+++ datatypes.py
@@ -1,16 +1,6 @@
-my_first_string = "Hello, World!"
-my_second_string = "42"
-my_first_integer = 42
+good_data = True
+some_data = True
+bad_data = False
 
-#print (my_first_string+ " " + my_second_string)
-#print (type(my_second_string))
-
-#print (f'My First string was "{my_first_string}". My second string was "{my_second_string}"')
-
-my_multiline_string = """This is a multiline string.
-It can span multiple lines.
-You can use it to write longer text without worrying about line breaks.
-""" 
-
-#print(my_multiline_string)
-print (str(my_first_integer) + " " + my_first_string) #Convert integer to string and concatenate
+print (good_data == bad_data)
+print (good_data == some_data)
