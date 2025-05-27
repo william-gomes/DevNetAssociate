bb642c6ffd82f3f2f357944fd630176517fa5de7
William Gomes <william.rko@gmail.com>
2025-05-26 22:02:29 -0300

    Trabalhando com strings

--- datatypes.py
+++ datatypes.py
@@ -1,8 +1,16 @@
-my_first_int = 42
-my_second_int = 100
-my_first_float = 3.14
-my_second_float = 2.718
+my_first_string = "Hello, World!"
+my_second_string = "42"
+my_first_integer = 42
 
-print (my_first_float+my_second_float)
-print (my_first_int+my_second_float)
-print (type(my_first_float+my_first_int))
+#print (my_first_string+ " " + my_second_string)
+#print (type(my_second_string))
+
+#print (f'My First string was "{my_first_string}". My second string was "{my_second_string}"')
+
+my_multiline_string = """This is a multiline string.
+It can span multiple lines.
+You can use it to write longer text without worrying about line breaks.
+""" 
+
+#print(my_multiline_string)
+print (str(my_first_integer) + " " + my_first_string) #Convert integer to string and concatenate
