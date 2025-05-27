cc715b6e03ea6095863181ab2476bb5b8fb5f8b7
William Gomes <william.rko@gmail.com>
2025-05-26 23:50:28 -0300

    Commando match

--- control_flow_logical.py
+++ control_flow_logical.py
@@ -1,49 +1,55 @@
-meus_carros = [
-        {
-        "marca": "Jeep",
-        "modelo": "Compass",
-        "ano": 2022,
-        "preco_pago": 140000.00,
-        "vendido": False,
-        "caracteristicas": {
-            "cor": "Prata",
-            "transmissao": "Automática",
-            "combustivel": "Flex"
-        }
-    },
-    {
-        "marca": "Honda",
-        "modelo": "City",
-        "ano": 2013,
-        "preco_pago": 51000.00,
-        "vendido": True,
-        "caracteristicas": {
-            "cor": "Prata",
-            "transmissao": "Automática",
-            "combustivel": "Flex"
-        }
-    },
-    {
-        "marca": "Fiat",
-        "modelo": "UNO",
-        "ano": 2000,
-        "preco_pago": 10000.00,
-        "vendido": True,
-        "caracteristicas": {
-            "cor": "Vinho",
-            "transmissao": "Manual",
-            "combustivel": "Gasolina"
-        }
-    }
-]
+# meus_carros = [
+#     {
+#         "marca": "Jeep",
+#         "modelo": "Compass",
+#         "ano": 2022,
+#         "preco_pago": 140000.00,
+#         "vendido": False,
+#         "caracteristicas": {
+#             "cor": "Prata",
+#             "transmissao": "Automática",
+#             "combustivel": "Flex"
+#         }
+#     },
+#     {
+#         "marca": "Honda",
+#         "modelo": "City",
+#         "ano": 2013,
+#         "preco_pago": 51000.00,
+#         "vendido": True,
+#         "caracteristicas": {
+#             "cor": "Prata",
+#             "transmissao": "Automática",
+#             "combustivel": "Flex"
+#         }
+#     },
+#     {
+#         "marca": "Fiat",
+#         "modelo": "UNO",
+#         "ano": 2000,
+#         "preco_pago": 10000.00,
+#         "vendido": True,
+#         "caracteristicas": {
+#             "cor": "Vinho",
+#             "transmissao": "Manual",
+#             "combustivel": "Gasolina"
+#         }
+#     }
+# ]
+code = 210
+match code:
+    case 404:
+        print("Not Found")
+    case 401:
+        print("Unauthorized")
+    case 403:
+        print("Forbidden")
+    case 301:
+        print("Moved Permanently")
+    case 500:
+        print("Internal Server Error")
+    case 200:
+        print("Success")
+    case _: # Default case for any other status code
+        print("Unknown Status Code")
 
-if meus_carros == []:
-    print("Nenhum carro cadastrado.")
-    print("Você pode cadastrar um carro agora.")
-    print("Obrigado!")
-elif meus_carros[0]["marca"] == "Jeep":
-    print("O primeiro carro é um Jeep.")
-    print (meus_carros[0]["marca"]+" "+meus_carros[0]["modelo"]+" "+str(meus_carros[0]["ano"]))
-else:
-    print("Há Carros cadastrados.")
-    print(meus_carros[0]["marca"]+" "+meus_carros[0]["modelo"])
