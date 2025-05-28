c612204c279bc19fb6012d93196bc9c15c40f9ae
William Gomes <william.rko@gmail.com>
2025-05-28 00:27:32 -0300

    Utilizando o try

--- control_flow_logical.py
+++ control_flow_logical.py
@@ -53,13 +53,42 @@ meus_carros = [
 #     case _: # Default case for any other status code
 #         print("Unknown Status Code")
 
-for carro in meus_carros:
-    print(carro["marca"], carro["modelo"], carro["ano"])
-    if carro["marca"] == "Jeep": 
-        print("Jeep encontrado! Boa compra!")
-        break
-    else :
-        continue
+meus_carros[0] = None
+
+# # Exemplo de uso de try/except para tratamento de erros
+# for carro in meus_carros:    
+#     try:
+#         print(carro["marca"], carro["modelo"], carro["ano"])
+#         if carro["marca"] == "Jeep": 
+#             print("Jeep encontrado! Boa compra!")
+#             break #Se encontrar um Jeep, encerra o loop, nao importa o resto
+#         else:
+#             continue #Continua o loop para o próximo carro, mas não executa o restante do código abaixo
+#     except TypeError as e:
+#         print("Encontrei um erro")
+#         print(e)
+#     else:
+#         print("Nenhum erro encontrado.")
+#     print("Teste para mostrar que break e continue funcionam") #Nao imprime porque o loop é interrompido pelo break e o continue não executa o restante do for
+
+# print("Fim do loop") #Executa normalmente porque está fora do loop
+
+# Exemplo de uso de try/except para tratamento de erros
+for carro in meus_carros:    
+    try: #Tenta executar o código dentro do bloco try
+        print(carro["marca"], carro["modelo"], carro["ano"])
+        if carro["marca"] == "Jeep": 
+            print("Jeep encontrado! Boa compra!")
+        else:
+            print("Carro não é um Jeep, continuando...")
+    except TypeError as e: # Captura o erro de tipo, caso o carro seja None
+        print("Encontrei um erro")
+        print(e)
+    else: # Se não houver erro, executa este bloco
+        print("Nenhum erro encontrado.")
+    finally:  # Este bloco é sempre executado, independentemente de erro ou não
+        print("O código continua aqui, independentemente")
+
 
 # if any(carro["vendido"] for carro in meus_carros):
 #     print("Alguns carros já foram vendidos.")
