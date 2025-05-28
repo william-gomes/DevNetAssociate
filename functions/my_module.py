5ffddcf35857f7062b5648feb10228fdd7a84864
William Gomes <william.rko@gmail.com>
2025-05-28 18:29:29 -0300

    Comecando com functions e classes

---
+++ functions/my_module.py
@@ -0,0 +1,7 @@
+def cube_number(input_number: int) -> int:
+    cubed = input_number ** 3
+    return cubed
+
+def greet_person (name: str) -> str:
+    return f"Hello, {name}!"
+
---
+++ functions_classes.py
@@ -0,0 +1,4 @@
+from functions.my_module import cube_number, greet_person
+
+print (cube_number(3))  # Output: 27
+print (greet_person("Alice"))  # Output: Hello, Alice!
