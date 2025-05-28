227000173f9a39192cbd2c546d56ebe2c36c75a9
William Gomes <william.rko@gmail.com>
2025-05-28 19:11:23 -0300

    Criando Classes

---
+++ classes/my_classes.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self, marca: str, modelo: str, ano: int, km: int, condicao: str, cor: str) -> None:
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.km = km
+        self.condicao = condicao
+        self.cor = cor
+        
--- functions_classes.py
+++ functions_classes.py
@@ -1,4 +1,11 @@
-from functions.my_module import cube_number, greet_person
+from classes.my_classes import Car
 
-print (cube_number(3))  # Output: 27
-print (greet_person("Alice"))  # Output: Hello, Alice!
+my_car = Car(marca="Jeep", modelo="Compass", ano=2022, km=31500, condicao="Semi Novo", cor="Cinza") #Argumentos nomeados (como marca="Jeep") não dependem da ordem.
+my_wife_car = Car("Honda", "City", 2013, 95000, "Usado", "Cinza Claro") #Argumentos posicionais (como "Honda") dependem da ordem. Se colocar o argumento errado, pode dar erro ou criar um objeto com atributos errados.
+#Resumindo, escolha entre argumentos nomeados, é a melhor prática, pois é mais legível e menos propenso a erros.
+
+# print(my_car) #Vai imprimir o objeto
+# print (my_car.marca) #Vai imprimir o atributo marca do objeto
+# print(my_car.__dict__) #Vai imprimir os atributos do objeto 
+
+print(my_wife_car.__dict__)
